00e6729ec201e6bd6bc9eff245fc9bc5eb08d337
Wei Alexander Xin <eggrollofchaos@gmail.com>
2021-04-16 15:41:17 -0400

    course materials

---
+++ .secrets/keys.py
@@ -0,0 +1,1 @@
+{"client_id" : "-pEsRVwee9viyT1bikndGw", "key":"im52ihxsXuFoD4gx15l2B-t90TL1xqTU-ysKPFEKrqtnB_CC7KgfByoR2u8ycdBnuJZolU4LCL7E3sVEYUoh0tRQ5d9P2V791_ADihXSTIHrJYQNf3j6XMZCFzIkYHYx"}
--- Phase_3/Calculator_class/descriptive.py
+++ Phase_3/Calculator_class/descriptive.py
@@ -5,25 +5,51 @@ Created on Wed Sep  4 14:25:52 2019
 
 @author: swilson5
 """
-import math
 
 
 class Calculator:
-    def __init__(self, data):
-        self.data = data;
+    def __init__(self, dataList):
+        self.data = dataList
+        self.length = len(self.data)
         self.mean = self.__calc_mean()
-
+        self.median = self.__calc_median()
+        self.variance = self.__calc_variance()
+        self.stand_dev = self.__calc_std()
 
     def __calc_mean(self):
-        mean = sum(self.data)/len(self.data)
+        mean = sum(self.data)/self.length
         return mean
 
-    def add_data(self, data):
-        self.data.append(data)
+    def __calc_median(self):
+        sorted_data = sorted(self.data)
+        if self.length % 2: # odd number of elements
+            median = sorted_data[self.length//2]
+        else:
+            median = (sorted_data[self.length//2 - 1] + sorted_data[self.length//2]) / 2
+        return median
+
+    def __calc_variance(self):
+        variance = sum([(i - self.mean)**2 for i in self.data])/ (self.length - 1)
+        return variance
+
+    def __calc_std(self):
+        std = (self.variance)**(0.5)
+        return std
+
+    def add_data(self, dataList):
+        self.data.extend(dataList)
+        self.length += len(dataList)
         self.mean = self.__calc_mean()
+        self.median = self.__calc_median()
+        self.variance = self.__calc_variance()
+        self.stand_dev = self.__calc_std()
 
-    pass        
+    def remove_data(self, dataList):
+        self.data.remove(*dataList)
+        self.length -= len(dataList)
+        self.mean = self.__calc_mean()
+        self.median = self.__calc_median()
+        self.variance = self.__calc_variance()
+        self.stand_dev = self.__calc_std()
 
-
-
-
+    # pass
